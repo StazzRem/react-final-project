d396bcfb530e1a251cd176ab690c685e8ba777ca
StazzRem <stacey.remnant@gmail.com>
2022-11-07 10:31:00 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,36 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <button type="button" class="btn btn-primary">
-        Click Here
-      </button>
+      <footer>
+        This page was built by{" "}
+        <a
+          href="https://www.linkedin.com/in/stacey-remnant-79338122a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Stacey Remnant
+        </a>
+        <br />
+        <a
+          href="https://github.com/StazzRem/react-final-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub Open Source
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://www.graphberry.com/category/icons"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Icons by Graphberry
+        </a>{" "}
+        |{" "}
+        <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+          Hosted by Netlify
+        </a>
+      </footer>
     </div>
   );
 }
